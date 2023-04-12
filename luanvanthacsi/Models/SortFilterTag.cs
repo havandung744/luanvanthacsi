351b9fb7c9ecc7a5a94471192edf9de7e82c843c
Hà Dũng <hdung72267@gmail.com>
2023-04-12 11:44:55 +0700

    thêm filter

---
+++ luanvanthacsi/Models/SortFilterTag.cs
@@ -0,0 +1,23 @@
+﻿namespace luanvanthacsi.Models
+{
+    public class SortFilterTag
+    {
+        public string FieldName { get; set; }
+        public string FieldDisplayName { get; set; }
+        public SortFilterTagType Type { get; set; }
+        public string Value { get; set; }
+        public string Direction { get; set; }
+        public string DirectionDisplay { get => Direction == "ascend" ? "tăng dần" : Direction == "descend" ? "giảm dần" : ""; }
+        public string Operator { get; set; }
+        public string OperatorDisplay { get; set; }
+        public string FilterText { get => $"{FieldDisplayName} {OperatorDisplay?.ToLower()} '{Value}'"; }
+        public string SortText { get => $"{FieldDisplayName} {DirectionDisplay}"; }
+        public int Index { get; set; }
+    }
+
+    public enum SortFilterTagType
+    {
+        Sort,
+        Filter
+    }
+}
